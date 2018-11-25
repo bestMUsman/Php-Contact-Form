33aee7ceefcafc19db1c103965a65a4cec936ae8
Muhammad Usman <musmanraodev@gmail.com>
2018-11-25 00:48:24 -0500

    fixed css

--- form/mail_handler.php
+++ form/mail_handler.php
@@ -1,25 +1,26 @@
-<style>    
-  body {
-    display: flex;
-  }
-  .mail-sent {
-    padding: 20px;
-    color: white;
-    background-color: #4caf50;
-    display: flex;
-    justify-content: center;
-    align-self: center;
-    width: 100%;
-    font-weight: 800;
-  }
+<style>
+body {
+  display: flex;
+}
+
+.mail-sent {
+  padding: 20px;
+  color: white;
+  background-color: #4caf50;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  width: 100%;
+  font-weight: 800;
+}
 </style>
 
 <?php 
 if(isset($_POST['submit'])){
 
-    $to = "askflow1111@gmail.com"; 
+    $to = "muhammadusmancoder@gmail.com"; // this is your Email address
 
-    $from = $_POST['email'];
+    $from = $_POST['email']; // this is the sender's Email address
 
     $name = $_POST['name'];
 
@@ -37,8 +38,14 @@ if(isset($_POST['submit'])){
 
     mail($to,$subject,$message,$headers);
 
-    mail($from,$subject2,$message2,$headers2); 
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
 
     echo "<p class='mail-sent'>Mail Sent. Thank you " . $name . ", we will contact you shortly.</p>";
+
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+
+    // You cannot use header and echo together. It's one or the other.
+
     }
+
 ?>
